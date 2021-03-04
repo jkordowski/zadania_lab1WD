41088244caa0a21b348ab8138035303fec84b215
JK <jakub.kordowski@student.uwm.edu.pl>
2021-03-04 13:58:59 +0100

    zadanie1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# Zad1. Napisz pierwszy skrypt, w którym zadeklarujesz po dwie zmienne każdego typu a następnie wyświetl te zmienne
 
+integer1 = 7
+integer2 = 39
+print(integer1, integer2)
+
+float1 = 7.4
+float2 = 39.9995
+print(float1, float2)
+
+string1 = 'Siedem'
+string2 = 'Ala ma kota'
+print(string1,string2)
+
+complex1 = 7+39j
+complex2 = 39+7j
+print(complex1,complex2)
---
+++ zad2.py
@@ -0,0 +1,3 @@
+# Zad2. Stwórz skrypt kalkulator, w którym wykorzystać wszystkie podstawowe działania arytmetyczne
+
+a, b, c, d = 1, 2, 3, 4
