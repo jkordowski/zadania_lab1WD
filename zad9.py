01b65d9320bff21fc0dc18045c8b980e022144d8
JK <jakub.kordowski@student.uwm.edu.pl>
2021-03-06 20:22:06 +0100

    zadanie9

--- zad9.py
+++ zad9.py
@@ -1,2 +1,10 @@
 #Zad.9 Napisz skrypt, w którym zadeklarujesz zmienne typu: string, float i szestnastkowe.
 # Następnie wyświetl je wykorzystując odpowiednie formatowanie.
+
+string = 'Hello World!'
+float = float(10)
+szesznastka = hex(156)
+
+print(string)
+print(float)
+print(szesznastka)
