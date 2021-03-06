83f9e63b700a6300084935801d2cf28bdcf7ee57
JK <jakub.kordowski@student.uwm.edu.pl>
2021-03-06 20:24:51 +0100

    zadanie4

--- zad4.py
+++ zad4.py
@@ -1,7 +1,12 @@
 # Zad4. Napisz skrypt, który policzy i wyświetli następujące wyrażenia:
 from math import *
+
 e10 = exp(10)
 print(e10)
 
 pierwiastek = pow((log10(5+pow(sin(8),2))),1/6)
 print(pierwiastek)
+
+print(floor(3.55))
+
+print(ceil(4.80))
